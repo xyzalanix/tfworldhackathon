5102bc238d4d5b6f1afe0804b0785140aed7dc3b
xyzalanix <aaixbazz@gmail.com>
2020-07-19 13:55:03 -0500

    fork
    moví hyperparameters, incluí alanBounces results

--- scripts/WGAN-GP.py
+++ scripts/WGAN-GP.py
@@ -17,21 +17,21 @@ tf.config.experimental.set_memory_growth(gpus[0], True)
 
 # Define hyperparameters
 MODEL_DIMS = 64
-NUM_SAMPLES = 16384
+NUM_SAMPLES = 65536
 D_UPDATES_PER_G_UPDATE = 5
 GRADIENT_PENALTY_WEIGHT = 10.0
 NOISE_LEN = 100
-EPOCHS = 128
+EPOCHS = 500
 EPOCHS_PER_SAMPLE = 2
 BATCH_SIZE = 16
 Fs = 16000
 
-DATA_DIR = r"D:\ML_Datasets\violin"
-INSTRUMENT = "violin"
+DATA_DIR = "losojos"
+INSTRUMENT = "losojos"
 
 print("Creating necessary directories")
 
-paths = ["logs/train", 
+paths = ["logs/train",
          f"models/{INSTRUMENT}/js",
          f"output/{INSTRUMENT}",]
 
@@ -41,25 +41,25 @@ for path in paths:
 
 # Define class that contains GAN infrastructure
 class GAN:
-    def __init__(self, model_dims=MODEL_DIMS, num_samples=NUM_SAMPLES, 
+    def __init__(self, model_dims=MODEL_DIMS, num_samples=NUM_SAMPLES,
                  gradient_penalty_weight=GRADIENT_PENALTY_WEIGHT, instrument=INSTRUMENT,
                  noise_len=NOISE_LEN, batch_size=BATCH_SIZE, sr=Fs):
         self.model_dims = model_dims
         self.num_samples = num_samples
         self.noise_dims = (noise_len,)
         self.batch_size = batch_size
-        
+
         self.G = GANModels.Generator(self.model_dims, num_samples)
         print(self.G.summary())
 
         self.D = GANModels.Critic(self.model_dims, num_samples)
         print(self.D.summary())
-        
+
         self.G_optimizer = Adam(learning_rate=1e-4, beta_1=0.5, beta_2=0.9)
         self.D_optimizer = Adam(learning_rate=1e-4, beta_1=0.5, beta_2=0.9)
-        
+
         self.gradient_penalty_weight = gradient_penalty_weight
-        
+
         self.sr = sr
 
         self.instrument = INSTRUMENT
@@ -69,7 +69,7 @@ class GAN:
         r_loss = - tf.reduce_mean(r_logit)
         f_loss = tf.reduce_mean(f_logit)
         return r_loss, f_loss
-    
+
     # Loss function for generator
     def _g_loss_fn(self, f_logit):
         f_loss = - tf.reduce_mean(f_logit)
@@ -83,18 +83,18 @@ class GAN:
             inter = a + alpha * (b - a)
             inter.set_shape(a.shape)
             return inter
-            
+
         x = _interpolate(real, fake)
         with tf.GradientTape() as t:
             t.watch(x)
             pred = self.D(x, training=True)
-            
+
         grad = t.gradient(pred, x)
         norm = tf.norm(tf.reshape(grad, [tf.shape(grad)[0], -1]), axis=1)
         gp = tf.reduce_mean((norm - 1.)**2)
 
         return gp
-        
+
     # Trains generator by keeping critic constant
     @tf.function
     def train_G(self):
@@ -128,19 +128,19 @@ class GAN:
         self.D_optimizer.apply_gradients(zip(D_grad, self.D.trainable_variables))
 
         return {'d_loss': x_real_d_loss + x_fake_d_loss, 'gp': gp}
-        
+
     # Creates music samples and saves current generator model
     def sample(self, epoch, num_samples=10):
-        self.G.save(f"models/{epoch}.h5")
+        self.G.save(f"models/{INSTRUMENT}/{epoch}.h5")
         z = tf.random.normal(shape=(num_samples,) + self.noise_dims)
         result = self.G(z, training=False)
         for i in range(num_samples):
             audio = result[i, :, :]
             audio = np.reshape(audio, (self.num_samples,))
-            librosa.output.write_wav(f"output/{self.instrument}/{epoch}-{i}.wav", 
+            librosa.output.write_wav(f"output/{self.instrument}/{epoch}-{i}.wav",
                                      audio, sr=self.sr)
 
-    
+
 ###############################################################################################
 
 # Instantiate model
@@ -167,13 +167,13 @@ print(f"X_train shape = {(len(X_train),) + X_train[0].shape}")
 
 # Save some random training data slices and create baseline generated data for comparison
 for i in range(10):
-    librosa.output.write_wav(f"output/{INSTRUMENT}/real-{i}.wav", 
+    librosa.output.write_wav(f"output/{INSTRUMENT}/real-{i}.wav",
                              X_train[random.randint(0, len(X_train) - 1)], sr=Fs)
 
 gan.sample("fake")
 
 train_summary_writer = tf.summary.create_file_writer("logs/train")
-    
+
 # Train GAN
 with train_summary_writer.as_default():
     steps_per_epoch = len(X_train) // BATCH_SIZE
@@ -181,25 +181,25 @@ with train_summary_writer.as_default():
     for e in range(EPOCHS):
         for i in range(steps_per_epoch):
             D_loss_sum = 0
-        
+
             # Update dcritic a set number of times for each update of the generator
             for n in range(D_UPDATES_PER_G_UPDATE):
                 gan.D.reset_states()
                 D_loss_dict = gan.train_D(np.array(random.sample(X_train, BATCH_SIZE)))
                 D_loss_sum += D_loss_dict['d_loss']
-            
+
             # Calculate average loss of critic for current step
             D_loss = D_loss_sum / D_UPDATES_PER_G_UPDATE
-            
+
             G_loss_dict = gan.train_G()
             G_loss = G_loss_dict['g_loss']
-        
+
             # Write logs
             tf.summary.scalar('d_loss', D_loss, step=(e*steps_per_epoch)+i)
             tf.summary.scalar('g_loss', G_loss, step=(e*steps_per_epoch)+i)
-        
+
             print(f"step {(e*steps_per_epoch)+i}: d_loss = {D_loss} g_loss = {G_loss}")
-        
+
         # Periodically sample generator
         if e % EPOCHS_PER_SAMPLE == 0:
             gan.sample(e)
